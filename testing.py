3a40e94d51ac63211b9be6f2f0ab5aae193d390e
Moussa <moussak10@gmail.com>
2023-10-22 22:35:52 -0300

    Fixed bug for token limit

--- testing.py
+++ testing.py
@@ -10,8 +10,19 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 import os
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
 os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
 
+
 llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-print(llm)
+
+prompt = 'what team won the soccer world cup in {year}'
+
+conversation_chain = LLMChain(
+    llm=llm,
+    prompt=PromptTemplate.from_template(prompt)
+)
+
+print(conversation_chain("2010"))
